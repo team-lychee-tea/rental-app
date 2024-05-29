7044191f976db4e1cedf2144609611676053f820
Yuki <134689144+yuki10000@users.noreply.github.com>
2024-05-29 14:43:03 +0900

    add a sample message

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Hello, let's make a rental app!!</p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
